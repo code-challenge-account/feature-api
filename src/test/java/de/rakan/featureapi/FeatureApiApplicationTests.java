48f01a42a247081400053e9e25fa7c5042b93e93
RD <code.3d.channel@gmail.com>
2020-11-10 21:04:26 +0100

    Task-101 Add spring project skeleton files

---
+++ src/test/java/de/rakan/featureapi/FeatureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rakan.featureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
