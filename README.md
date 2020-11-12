6942534c6acc120b977e864c3c72ea6b8031b6f2
RD <code.3d.channel@gmail.com>
2020-11-12 22:58:02 +0100

    Task-101 Add Swagger

--- README.md
+++ README.md
@@ -13,6 +13,10 @@ You can check if the application is up and running by executing:
 curl localhost:8085/actuator/health
 ```
 
+### Inspecting the API 
+
+If you want to inspect the API or try things out, you can use the Swagger UI at http://localhost:8085/swagger-ui/
+
 #### Debugging
 
 Remote debugging during application execution can be set up by attaching to the port 5010 using your favourite IDE, i.e. IntelliJ IDEA.
--- src/main/java/de/rakan/featureapi/Main.java
+++ src/main/java/de/rakan/featureapi/Main.java
@@ -6,8 +6,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Main {
 
 	public static void main(String[] args) {
@@ -17,6 +23,15 @@ public class Main {
 	@Bean
 	public FeatureCache featureCache(@Value("${SOURCE_DATA_FILE_NAME}") String sourceDataFileName) {
 		return new StaticFeatureCache(sourceDataFileName);
+	}
+
+	@Bean
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
 	}
 
 }
